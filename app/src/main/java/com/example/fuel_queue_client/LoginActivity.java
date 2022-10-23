864edd187585591437d00c7bdcc30199454f77c9
Thushara Thiwanka <thusharathiwanka.dev@gmail.com>
2022-10-23 14:10:48 +0530

    CREATE: login credentials managed

--- app/src/main/java/com/example/fuel_queue_client/LoginActivity.java
+++ app/src/main/java/com/example/fuel_queue_client/LoginActivity.java
@@ -2,17 +2,106 @@ package com.example.fuel_queue_client;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Switch;
+import android.widget.Toast;
 
 import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class LoginActivity extends AppCompatActivity {
+    ImageView backBtn;
+    Switch stationOwnerSwitch;
+    Button loginBtn;
+    EditText usernameInput, passwordInput;
 
+    String username;
+    String password;
+    String role = "customer";
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Objects.requireNonNull(getSupportActionBar()).hide();
-
         setContentView(R.layout.activity_login);
+
+        backBtn = findViewById(R.id.back);
+        loginBtn = findViewById(R.id.loginBtn);
+        usernameInput = findViewById(R.id.emailInput);
+        passwordInput = findViewById(R.id.passwordInput);
+        stationOwnerSwitch = findViewById(R.id.ownerSwitch);
+
+
+//        final DBHelperProfile loginDB = new DBHelperProfile(this);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                username = usernameInput.getText().toString();
+                password = passwordInput.getText().toString();
+
+                System.out.println(username + password + role);
+
+                if (username.length() <= 0 || password.length() <= 0) {
+                    Toast.makeText(LoginActivity.this, "Please fill all the fields", Toast.LENGTH_SHORT).show();
+                } else {
+                    boolean isValidUsername = usernameValidate(username);
+
+                    if (isValidUsername) {
+                        if (true) {
+                            Intent intent = new Intent(getApplicationContext(), UserProfileActivity.class);
+                            intent.putExtra("USERNAME", username);
+                            startActivity(intent);
+                            Toast.makeText(LoginActivity.this, "Welcome back " + username, Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Your username or password is invalid", Toast.LENGTH_SHORT).show();
+                        }
+                    } else {
+                        Toast.makeText(LoginActivity.this, "Enter a valid username", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        stationOwnerSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked) {
+                    role = "station-owner";
+                }
+            }
+        });
+    }
+
+    public static boolean emailValidate (String email){
+        String emailRegex = "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$";
+        Pattern emailPattern = Pattern.compile(emailRegex, Pattern.CASE_INSENSITIVE);
+        Matcher emailMatcher = emailPattern.matcher(email);
+
+        return emailMatcher.find();
+    }
+
+    public static boolean usernameValidate (String username){
+        String usernameRegex = "^[aA-zZ0-9_-]\\w{5,30}$";
+        Pattern usernamePattern = Pattern.compile(usernameRegex);
+        Matcher usernameMatcher = usernamePattern.matcher(username);
+
+        return usernameMatcher.matches();
     }
 }
