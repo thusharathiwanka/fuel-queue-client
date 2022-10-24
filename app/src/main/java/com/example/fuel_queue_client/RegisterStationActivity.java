2dfad9ce7412a15c7d514902561193ec24fd0c72
Weerasinghe D.N.H <it19070704@my.sliit.lk>
2022-10-25 01:02:46 +0530

    UPDATE : Station list view  connected to backend

--- app/src/main/java/com/example/fuel_queue_client/RegisterStationActivity.java
+++ app/src/main/java/com/example/fuel_queue_client/RegisterStationActivity.java
@@ -7,10 +7,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.Switch;
 import android.widget.Toast;
 
 import com.example.fuel_queue_client.api.APIConfig;
@@ -28,16 +26,15 @@ public class RegisterStationActivity extends AppCompatActivity {
 
     ImageView backBtn;
     Button regBtn;
-    EditText Registration_numberInput, stationNameInput, locationInput,noOfPumps,ArrivalTimeInput,FinishTimeInput;
-    Switch availability_switch;
+    EditText Registration_numberInput, stationNameInput, locationInput,noOfPumps;
 
     String registrationNumber;
     String name;
     String location;
     String noPumps;
-    String availability;
-    String arrivalTime;
-    String finishTime;
+    String availability = "";
+    String arrivalTime ="";
+    String finishTime ="";
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -51,18 +48,14 @@ public class RegisterStationActivity extends AppCompatActivity {
         stationNameInput = findViewById(R.id.stationNameInput);
         locationInput = findViewById(R.id.locationInput);
         noOfPumps = findViewById(R.id.noOfPumps);
-        availability_switch = findViewById(R.id.availability_switch);
-        ArrivalTimeInput = findViewById(R.id.ArrivalTimeInput);
-        FinishTimeInput = findViewById(R.id.FinishTimeInput);
+
 
         regBtn.setOnClickListener(view -> {
             registrationNumber = Registration_numberInput.getText().toString();
             name = stationNameInput.getText().toString();
             location = locationInput.getText().toString();
             noPumps = noOfPumps.getText().toString();
-            availability = availability_switch.getText().toString();
-            arrivalTime = ArrivalTimeInput.getText().toString();
-            finishTime = FinishTimeInput.getText().toString();
+
 
             IFuelStationApi fuelStationApi = APIConfig.getConfig().create(IFuelStationApi.class);
             Call<FuelStationResponse> call = fuelStationApi.registerStation(new FuelStationRequest( registrationNumber,name,location,noPumps,availability,arrivalTime,finishTime));
@@ -92,12 +85,6 @@ public class RegisterStationActivity extends AppCompatActivity {
 
         });
 
-        availability_switch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                    availability = String.valueOf(isChecked);
-
-            }
-        });
         backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
