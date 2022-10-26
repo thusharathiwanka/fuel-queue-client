33101a9242264a4cc89c13613a9f53894a209d70
Weerasinghe D.N.H <it19070704@my.sliit.lk>
2022-10-26 20:07:35 +0530

    UPDATE : updated two list components with comments

--- app/src/main/java/com/example/fuel_queue_client/CustomerProfileActivity.java
+++ app/src/main/java/com/example/fuel_queue_client/CustomerProfileActivity.java
@@ -16,6 +16,7 @@ import java.util.Objects;
 public class CustomerProfileActivity extends AppCompatActivity {
 
     Button View_list, Logout;
+
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,21 +27,26 @@ public class CustomerProfileActivity extends AppCompatActivity {
         View_list = findViewById(R.id.View_station_customer);
         Logout  = findViewById(R.id.LogOut_Customer);
 
+        // directs to the available fuel station list
         View_list.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(CustomerProfileActivity.this, AvailableSationListActivity.class);
+                Intent intent = new Intent(CustomerProfileActivity.this, AvailableStationListActivity.class);
                 startActivity(intent);
             }
         });
 
+        // directs to the main page and deletes the locally saved user object
         Logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //creates a db helper object for current activity
                 DBHelper dbHelper = new DBHelper(CustomerProfileActivity.this);
 
+                //directs to the main activity
                 Intent intent = new Intent(CustomerProfileActivity.this, MainActivity.class);
                 User user = dbHelper.getSingleUser();
+                //deletes the locally saved user
                 dbHelper.deleteOne(user.getId());
                 startActivity(intent);
 
