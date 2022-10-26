66c7268f3590d80230d502ce817338c01b838074
Weerasinghe D.N.H <it19070704@my.sliit.lk>
2022-10-26 13:50:05 +0530

    UPDATE : UI modifications done in user profles and forms

--- app/src/main/java/com/example/fuel_queue_client/CustomerProfileActivity.java
+++ app/src/main/java/com/example/fuel_queue_client/CustomerProfileActivity.java
@@ -2,13 +2,50 @@ package com.example.fuel_queue_client;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.fuel_queue_client.database.DBHelper;
+import com.example.fuel_queue_client.models.user.User;
+
+import java.util.Objects;
 
 public class CustomerProfileActivity extends AppCompatActivity {
 
+    Button View_list, Logout;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Objects.requireNonNull(getSupportActionBar()).hide();
         setContentView(R.layout.activity_customer_profile);
+
+        View_list = findViewById(R.id.View_station_customer);
+        Logout  = findViewById(R.id.LogOut_Customer);
+
+        View_list.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CustomerProfileActivity.this, AvailableSationListActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DBHelper dbHelper = new DBHelper(CustomerProfileActivity.this);
+
+                Intent intent = new Intent(CustomerProfileActivity.this, MainActivity.class);
+                User user = dbHelper.getSingleUser();
+
+                startActivity(intent);
+
+            }
+        });
+
     }
 }
